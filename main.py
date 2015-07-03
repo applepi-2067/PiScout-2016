9ecb69ffa4dc8082977831f8dfd38b61c83b5b5a
Cory Lynch <cjl2625@gmail.com>
2015-07-02 23:11:13 -0400

    Now can read data from the sheet

--- main.py
+++ main.py
@@ -1,63 +1,82 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+from piscout import *
 
-img = cv2.imread('sheetrotated.jpg')
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+scout = PiScout()
+scout.loadsheet('sheet.png')
 
-# The first step is to figure out the four markers on the corners of the page
-# The next two lines will blur the image and extract the edges from the shapes
-blur = cv2.GaussianBlur(imgray,(5,5),0)
-edges = cv2.Canny(blur,150,300)
+num1 = scout.rangefield('O5', 0, 9)
+num2 = scout.rangefield('O6', 0, 9)
+num3 = scout.rangefield('O7', 0, 9)
+num4 = scout.rangefield('O8', 0, 9)
 
-# Next, we use the edges to find the contours of the shapes
-# Once the contours are found, we use approxPolyDP to resolve the contours into polygon approximations
-# If the polygons have 4 sides and are relatively large, save the center coordinates in sq[]
-image, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-sq = []
-for cont in contours:
-		poly = cv2.approxPolyDP(np.array(cont), 3, True)
-		if len(poly) == 4 and cv2.contourArea(cont) > 2048:
-			xpos = 0; ypos = 0
-			for a in poly:
-				xpos += a[0][0]
-				ypos += a[0][1]
-			sq.append((int(xpos/4), int(ypos/4)))
+match1 = scout.rangefield('O10', 0, 1)
+match2 = scout.rangefield('O11', 0, 9)
+match3 = scout.rangefield('O12', 0, 9)
 
-# Here, we determine which four elements of sq[] are the marks
-# To do this, we iterate through each corner of the sheet
-# On each iteration, we find the element of sq[] with the shortest distance to the corner being examined
-marks = []
-h, w, c  = img.shape
-corners = [(0, 0), (0, h), (w, 0), (w,h)]
-for corner in corners:
-	marks.append(sq[np.argmin(list(map(lambda a : (corner[0] - a[0])**2 + (corner[1] - a[1])**2, sq)))])
+totes = scout.rangefield('J15', 1, 3)
+zonecont = scout.rangefield('J16', 1, 4)
+stepcont = scout.rangefield('J17', 1, 4)
 
-# Now, we fit apply a perspective transform
-# The centers of the 4 marks become the 4 corners of the image
-pts1 = np.float32(marks)
-pts2 = np.float32([[0,0],[0,700],[500,0],[500,700]])
-M = cv2.getPerspectiveTransform(pts1,pts2)
-dst = cv2.warpPerspective(img,M,(500,700))
-#plt.subplot(121),plt.imshow(img),plt.title('Input')
-#plt.subplot(122),plt.imshow(dst),plt.title('Output')
-#plt.show()
+stacked = scout.boolfield('W15')
+inzone = scout.boolfield('W16')
 
-# Apply a binary threshold
-img = dst
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+#The following wall of code could probably be done iteratively, but I'm too lazy to do that at the moment
+stacks = [None]*6
 
-# Detect filled boxes
-# This iterates through each grid location and picks the ones that are most dark
-data = [[None]*25]*35
-for row in range(34):
-	for col in range(24):
-		# I have no idea how the next line works, but it sums the pixels in the grid unit
-		a = [item[col*20+10:col*20+30] for item in img[row*20+10:row*20+30]]
-		if sum(map(sum, a)) < 25000:
-			img = cv2.circle(img, (col*20+20, row*20+20), 4, 127, -1)
+stacks[0] = {
+	'capping': scout.boolfield('L20'),
+	'height': scout.rangefield('G21', 1, 6),
+	'capped': scout.boolfield('G22'),
+	'noodled': scout.boolfield('L22')
+}
+stacks[1] = {
+	'capping': scout.boolfield('Y20'),
+	'height': scout.rangefield('T21', 1, 6),
+	'capped': scout.boolfield('T22'),
+	'noodled': scout.boolfield('Y22')
+}
+stacks[2] = {
+	'capping': scout.boolfield('L24'),
+	'height': scout.rangefield('G25', 1, 6),
+	'capped': scout.boolfield('G26'),
+	'noodled': scout.boolfield('L26')
+}
+stacks[3] = {
+	'capping': scout.boolfield('Y24'),
+	'height': scout.rangefield('T25', 1, 6),
+	'capped': scout.boolfield('T26'),
+	'noodled': scout.boolfield('Y26')
+}
+stacks[4] = {
+	'capping': scout.boolfield('L28'),
+	'height': scout.rangefield('G29', 1, 6),
+	'capped': scout.boolfield('G30'),
+	'noodled': scout.boolfield('L30')
+}
+stacks[5] = {
+	'capping': scout.boolfield('Y28'),
+	'height': scout.rangefield('T29', 1, 6),
+	'capped': scout.boolfield('T30'),
+	'noodled': scout.boolfield('Y30')
+}
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print("Team Number:")
+print(1000*num1 + 100*num2 + 10*num3 + num4)
+
+print("Match Number")
+print(100*match1 + 10*match2 + match3)
+
+print("Totes in auto zone: " + str(totes))
+print("Containers in auto zone: " + str(zonecont))
+print("Containers from step: " + str(stepcont))
+print("Stacked set? " + str(stacked))
+print("Moved into auto zone? " + str(inzone))
+
+for s in stacks:
+	if s['height'] == 0:
+		continue
+	if s['capping']:
+		print('Capped a stack of ' + str(s['height']))
+	else:
+		print('Built a stack of ' + str(s['height']))
+		print('Capped: ' + str(s['capped']))
+	print('Noodled: ' + str(s['noodled']))
---
+++ piscout.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+class PiScout:
+	def __init__(self):
+		self.sheet = None;
+
+	def loadsheet(self, imgpath):
+		img = cv2.imread(imgpath)
+		imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+		# The first step is to figure out the four markers on the corners of the page
+		# The next two lines will blur the image and extract the edges from the shapes
+		blur = cv2.GaussianBlur(imgray,(5,5),0)
+		edges = cv2.Canny(blur,150,300)
+
+		# Next, we use the edges to find the contours of the shapes
+		# Once the contours are found, we use approxPolyDP to resolve the contours into polygon approximations
+		# If the polygons have 4 sides and are relatively large, save the center coordinates in sq[]
+		image, contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		sq = []
+		for cont in contours:
+				poly = cv2.approxPolyDP(np.array(cont), 3, True)
+				if len(poly) == 4 and cv2.contourArea(cont) > 2048:
+					xpos = 0; ypos = 0
+					for a in poly:
+						xpos += a[0][0]
+						ypos += a[0][1]
+					sq.append((int(xpos/4), int(ypos/4)))
+
+		# Here, we determine which four elements of sq[] are the marks
+		# To do this, we iterate through each corner of the sheet
+		# On each iteration, we find the element of sq[] with the shortest distance to the corner being examined
+		marks = []
+		h, w, c  = img.shape
+		corners = [(0, 0), (0, h), (w, 0), (w,h)]
+		for corner in corners:
+			marks.append(sq[np.argmin(list(map(lambda a : (corner[0] - a[0])**2 + (corner[1] - a[1])**2, sq)))])
+
+		# Now, we fit apply a perspective transform
+		# The centers of the 4 marks become the 4 corners of the image
+		pts1 = np.float32(marks)
+		pts2 = np.float32([[0,0],[0,700],[500,0],[500,700]])
+		M = cv2.getPerspectiveTransform(pts1,pts2)
+		img = cv2.warpPerspective(img,M,(500,700))
+		#plt.subplot(121),plt.imshow(img),plt.title('Input')
+		#plt.subplot(122),plt.imshow(dst),plt.title('Output')
+		#plt.show()
+
+		#GUI testing
+		#fig = plt.figure()
+		#aa = fig.add_subplot(111)
+		#aa.imshow(img)
+		#aa.text(0,0,'memes!')
+		#plt.show()
+
+		# Apply a binary threshold
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		ret, self.sheet = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+
+	def viewsheet(self):
+		cv2.imshow("Loaded Sheet", self.sheet)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+	def getvalue(self, loc):
+		col,row = loc
+		box = [item[col*20+10:col*20+30] for item in self.sheet[row*20+10:row*20+30]]
+		return sum(map(sum, box))
+
+	def parse(self, loc):
+		col = loc[0]
+		row = loc[1:]
+		return (ord(col)-67, int(row)-3)
+
+	def boolfield(self, location):
+		loc = self.parse(location)
+		return self.getvalue(loc) < 25000
+
+	def rangefield(self, startlocation, startval, endval):
+		loc = self.parse(startlocation)
+		values = [self.getvalue((val, loc[1])) for val in range(loc[0], loc[0]+endval-startval+1)]
+		min = np.argmin(values)
+		if values[min] < 30000:
+			return startval + min
+		return 0
+'''
+# Detect filled boxes
+# This iterates through each grid location and picks the ones that are most dark
+data = [[None]*25]*35
+for row in range(34):
+	for col in range(24):
+		# I have no idea how the next line works, but it sums the pixels in the grid unit
+		a = [item[col*20+10:col*20+30] for item in img[row*20+10:row*20+30]]
+		if sum(map(sum, a)) < 25000:
+			img = cv2.circle(img, (col*20+20, row*20+20), 4, 127, -1)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
