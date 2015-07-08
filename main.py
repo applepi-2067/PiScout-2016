64482548fb0c417b0894ac99df6a83862f84af0d
Cory Lynch <cjl2625@gmail.com>
2015-07-08 17:31:42 -0400

    Complete double support, improved GUI, editing data

--- main.py
+++ main.py
@@ -1,112 +1,110 @@
 from piscout import *
 
 scout = PiScout()
-scout.loadsheet('double.jpg')
-num1 = scout.rangefield('O-6', 0, 9)
-num2 = scout.rangefield('O-7', 0, 9)
-num3 = scout.rangefield('O-8', 0, 9)
-num4 = scout.rangefield('O-9', 0, 9)
-scout.disp("Team Number: " + str(1000*num1 + 100*num2 + 10*num3 + num4))
-inzone = scout.boolfield('ag-12')
-scout.disp("Moved into auto zone: " + str(inzone))
-#scout.viewsheet()
+scout.loadsheet('double_new.jpg')
 
-'''
-num1 = scout.rangefield('O5', 0, 9)
-num2 = scout.rangefield('O6', 0, 9)
-num3 = scout.rangefield('O7', 0, 9)
-num4 = scout.rangefield('O8', 0, 9)
+for s in (0,23):
+	scout.shiftDown(s) #on the second iteration, it shifts down to see the second match
+	num1 = scout.rangefield('O-6', 0, 9)
+	num2 = scout.rangefield('O-7', 0, 9)
+	num3 = scout.rangefield('O-8', 0, 9)
+	num4 = scout.rangefield('O-9', 0, 9)
 
-match1 = scout.rangefield('O10', 0, 1)
-match2 = scout.rangefield('O11', 0, 9)
-match3 = scout.rangefield('O12', 0, 9)
+	match1 = scout.rangefield('O-11', 0, 1)
+	match2 = scout.rangefield('O-12', 0, 9)
+	match3 = scout.rangefield('O-13', 0, 9)
 
-totes = scout.rangefield('J15', 1, 3)
-zonecont = scout.rangefield('J16', 1, 4)
-stepcont = scout.rangefield('J17', 1, 4)
+	totes = scout.rangefield('AG-8', 1, 3)
+	zonecont = scout.rangefield('AG-9', 1, 4)
+	stepcont = scout.rangefield('AG-10', 1, 4)
 
-stacked = scout.boolfield('W15')
-inzone = scout.boolfield('W16')
+	stacked = scout.boolfield('AG-11')
+	inzone = scout.boolfield('AG-12')
 
-#The following wall of code could probably be done iteratively, but I'm too lazy to do that at the moment
-stacks = [None]*6
+	#The following wall of code could probably be done iteratively, but I'm too lazy to do that at the moment
+	stacks = [None]*6
 
-stacks[0] = {
-	'capping': scout.boolfield('L20'),
-	'height': scout.rangefield('G21', 1, 6),
-	'capped': scout.boolfield('G22'),
-	'noodled': scout.boolfield('L22')
-}
-stacks[1] = {
-	'capping': scout.boolfield('Y20'),
-	'height': scout.rangefield('T21', 1, 6),
-	'capped': scout.boolfield('T22'),
-	'noodled': scout.boolfield('Y22')
-}
-stacks[2] = {
-	'capping': scout.boolfield('L24'),
-	'height': scout.rangefield('G25', 1, 6),
-	'capped': scout.boolfield('G26'),
-	'noodled': scout.boolfield('L26')
-}
-stacks[3] = {
-	'capping': scout.boolfield('Y24'),
-	'height': scout.rangefield('T25', 1, 6),
-	'capped': scout.boolfield('T26'),
-	'noodled': scout.boolfield('Y26')
-}
-stacks[4] = {
-	'capping': scout.boolfield('L28'),
-	'height': scout.rangefield('G29', 1, 6),
-	'capped': scout.boolfield('G30'),
-	'noodled': scout.boolfield('L30')
-}
-stacks[5] = {
-	'capping': scout.boolfield('Y28'),
-	'height': scout.rangefield('T29', 1, 6),
-	'capped': scout.boolfield('T30'),
-	'noodled': scout.boolfield('Y30')
-}
+	stacks[0] = {
+		'capping': scout.boolfield('L-15'),
+		'height': scout.rangefield('G-16', 1, 6),
+		'capped': scout.boolfield('G-17'),
+		'noodled': scout.boolfield('L-17')
+	}
+	stacks[1] = {
+		'capping': scout.boolfield('X-15'),
+		'height': scout.rangefield('S-16', 1, 6),
+		'capped': scout.boolfield('S-17'),
+		'noodled': scout.boolfield('X-17')
+	}
+	stacks[2] = {
+		'capping': scout.boolfield('AJ-15'),
+		'height': scout.rangefield('AE-16', 1, 6),
+		'capped': scout.boolfield('AE-17'),
+		'noodled': scout.boolfield('AJ-17')
+	}
+	stacks[3] = {
+		'capping': scout.boolfield('L-19'),
+		'height': scout.rangefield('G-20', 1, 6),
+		'capped': scout.boolfield('G-21'),
+		'noodled': scout.boolfield('L-21')
+	}
+	stacks[4] = {
+		'capping': scout.boolfield('X-19'),
+		'height': scout.rangefield('S-20', 1, 6),
+		'capped': scout.boolfield('S-21'),
+		'noodled': scout.boolfield('X-21')
+	}
+	stacks[5] = {
+		'capping': scout.boolfield('AJ-19'),
+		'height': scout.rangefield('AE-20', 1, 6),
+		'capped': scout.boolfield('AE-21'),
+		'noodled': scout.boolfield('AJ-21')
+	}
 
-telecont = scout.rangefield('J32', 1, 4)
-coop = scout.rangefield('V32', 1, 4)
-coopstack = scout.boolfield('V33')
-toteloc = scout.rangefield('F36', 1, 18)
+	telecont = scout.rangefield('K-23', 1, 4)
+	coop = scout.rangefield('T-23', 1, 4)
+	coopstack = scout.boolfield('AI-23')
+	toteloc = scout.rangefield('L-26', 1, 18)
 
-scout.disp("Team Number: " + str(1000*num1 + 100*num2 + 10*num3 + num4))
-scout.disp("Match Number: " + str(100*match1 + 10*match2 + match3))
+	scout.set("team", 1000*num1 + 100*num2 + 10*num3 + num4)
+	scout.set("match", 100*match1 + 10*match2 + match3)
+	scout.set("auto totes", totes)
+	scout.set("RC in zone", zonecont)
+	scout.set("RC from step", stepcont)
+	scout.set("stacked set", stacked)
+	scout.set("moved to zone", inzone)
+	scout.set("stacks", [stack for stack in stacks if stack['height']])
+	scout.set("coop", coop)
+	scout.set("teleop step RC", telecont)
+	scout.set("coop stack?", coopstack)
+	scout.set("tote location", toteloc)
 
-scout.disp("Totes in auto zone: " + str(totes))
-scout.disp("Containers in auto zone: " + str(zonecont))
-scout.disp("Containers from step: " + str(stepcont))
-scout.disp("Stacked set? " + str(stacked))
-scout.disp("Moved into auto zone? " + str(inzone))
+	stackmsg = ''
+	for s in stacks:
+		if s['height'] == 0:
+			continue
+		if s['capping']:
+			stackmsg += '\nCapped a stack of ' + str(s['height'])
+		else:
+			stackmsg += '\nBuilt a stack of ' + str(s['height'])
+			stackmsg += '\nCapped: ' + str(s['capped'])
+		stackmsg += '\nNoodled: ' + str(s['noodled'])
+	scout.customdisp("stacks", stackmsg)
 
-for s in stacks:
-	if s['height'] == 0:
-		continue
-	if s['capping']:
-		scout.disp('Capped a stack of ' + str(s['height']))
+	locmsg = ''
+	if toteloc == 1:
+		locmsg = 'All from HP'
+	elif toteloc == 18:
+		locmsg = 'All from landfill'
+	elif 2 <= toteloc <= 7:
+		slocmsg = 'Most from HP'
+	elif 17 >= toteloc >= 12:
+		locmsg = 'Most from landfill'
+	elif toteloc == 0:
+		locmsg = "N\A"
 	else:
-		scout.disp('Built a stack of ' + str(s['height']))
-		scout.disp('Capped: ' + str(s['capped']))
-	scout.disp('Noodled: ' + str(s['noodled']))
+		locmsg = "Equal from HP/landfill"
+	scout.customdisp("tote location", locmsg)
+	scout.submit()
 
-scout.disp('Teleop containers from step: ' + str(telecont))
-scout.disp('Coop totes placed: ' + str(coop))
-scout.disp('Got coop stack? ' + str(coopstack))
 
-if toteloc == 1:
-	scout.disp('Gets all totes from HP')
-elif toteloc == 18:
-	scout.disp('Gets all totes from landfill')
-elif 2 <= toteloc <= 7:
-	scout.disp('Gets most totes from HP')
-elif 17 >= toteloc >= 12:
-	scout.disp('Gets most totes from landfill')
-elif toteloc == 0:
-	scout.disp("Doesn't pick up gray totes")
-else:
-	scout.disp("Gets totes equally from landfill/HP")
-'''
-scout.finish()
--- piscout.py
+++ piscout.py
@@ -1,11 +1,18 @@
 import cv2
+import os
 import numpy as np
+from ast import literal_eval
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Button
+from tkinter import messagebox
 
 class PiScout:
 	def __init__(self):
 		self.sheet = None;
-		self.output = ''
+		self.display = None;
+		self.output = {}
+		self.data = {}
+		self.shift = 0
 
 	# Loads a new scout sheet from an image
 	# Processes the image and stores the result in self.sheet
@@ -48,17 +55,19 @@ class PiScout:
 		pts2 = np.float32([[0,0],[0,784],[560,0],[560,784]])
 		M = cv2.getPerspectiveTransform(pts1,pts2)
 		img = cv2.warpPerspective(img,M,(560,784))
-
+		self.display = img.copy()
 		self.sheet = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		self.output = ''
 		print("Loading complete")
+
+	def shiftDown (self, amount):
+		self.shift = amount
 
 	# Opens the sheet in a new window
 	def viewsheet(self):
-		cv2.imshow("Loaded Sheet", self.sheet)
+		#img = self.sheet[48:384, 32:544]
+		cv2.imshow("Loaded Sheet", self.display)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
-
 
 	# Gets the shading value of a grid unit
 	# 0 is completely shaded, 102000 is completely unshaded
@@ -70,38 +79,84 @@ class PiScout:
 	# Parses a location in Letter-Number form and returns a tuple of the pixel coordinates
 	def parse(self, loc):
 		col,row = loc.upper().split('-')
-		return (ord(col)-66 if len(col)==1 else ord(col[1])-41, int(row)-3)
+		return (ord(col)-67 if len(col)==1 else ord(col[1])-41, self.shift + int(row)-3)
 
 	# Define a new boolean field at a given location
 	# Returns whether or not the grid unit is shaded
 	def boolfield(self, location):
 		loc = self.parse(location)
+		cv2.rectangle(self.display, (loc[0]*16, loc[1]*16), (loc[0]*16+16, loc[1]*16+16), (0,50,150),3)
 		return self.getvalue(loc) < 45000
 
 	# Define a new range field at a given location
-	# This field spans across grid units
+	# This field spans across multiple grid units
 	# Returns the shaded value, or 0 if none is shaded
 	def rangefield(self, startlocation, startval, endval):
 		loc = self.parse(startlocation)
-		values = [self.getvalue((val, loc[1])) for val in range(loc[0], loc[0]+endval-startval+1)]
+		end = loc[0]-startval+endval+1 #grid coordinate where the rangefield ends
+		cv2.rectangle(self.display, (loc[0]*16, loc[1]*16), (end*16, loc[1]*16+16), (0,50,150),3)
+		values = [self.getvalue((val, loc[1])) for val in range(loc[0], end)]
 		min = np.argmin(values)
 		if values[min] < 45000:
 			return startval + min
 		return 0
 
-	# Prepares a string to be displayed on the GUI
-	def disp(self, text):
-		self.output += str(text) + '\n'
+	def set(self, name, contents):
+		self.data[name] = contents
+		self.output[name] = contents
+
+	def customdisp(self, datakey, value):
+		self.output[datakey] = value
 
 	# Opens the GUI, including the sheet and the output text
-	def finish(self):
+	def submit(self):
 		print("Opening GUI")
-		plt.close()
-		img = cv2.cvtColor(self.sheet, cv2.COLOR_GRAY2BGR)
+		output = ''
+		for key,val in self.data.items():
+			output += "'" + key + "'" + ": " + str(val) + '\n'
+		output = output.replace(', ', '\n    ')
 		fig = plt.figure('PiScout')
 		fig.subplots_adjust(left=0, right=0.6)
 		plt.subplot(111)
-		plt.imshow(img)
+		plt.imshow(self.display)
 		plt.title('Scanned Sheet')
-		plt.text(600,784,self.output,fontsize=14)
+		plt.text(600,784,output,fontsize=14)
+		approve = Button(plt.axes([0.68, 0.24, 0.15, 0.07]), 'Submit Data')
+		approve.on_clicked(self.upload)
+		reject = Button(plt.axes([0.68, 0.17, 0.15, 0.07]), 'Edit Data')
+		reject.on_clicked(self.edit)
+		reject = Button(plt.axes([0.68, 0.1, 0.15, 0.07]), 'Cancel')
+		reject.on_clicked(self.cancel)
+		mng = plt.get_current_fig_manager()
+		mng.window.state('zoomed')
 		plt.show()
+
+		self.output = {}
+		self.data = {}
+		self.display = cv2.cvtColor(self.sheet, cv2.COLOR_GRAY2BGR)
+
+	def upload(self, event):
+		plt.close()
+
+	def edit(self, event):
+		datastr = ''
+		for key,val in self.data.items():
+			datastr += "'" + key + "'" + ": " + str(val) + "\n"
+		with open('piscout.txt', "w") as file:
+			file.write(datastr)
+		os.system('piscout.txt')
+		datastr = '{'
+		with open('piscout.txt', 'r') as file:
+			for line in file:
+				datastr += line.replace('\n', ', ')
+		datastr += "}"
+		try:
+			self.data = literal_eval(datastr)
+		except:
+			messagebox.showerror("Malformed Data", "You messed something up; the data couldn't be read. Try again.")
+		self.output = self.data
+		plt.close()
+		self.submit()
+
+	def cancel(self, event):
+		plt.close()
