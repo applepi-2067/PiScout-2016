ddbf191854cde896fcbb1528ba187193b8d02fcb
Cory Lynch <cjl2625@gmail.com>
2015-07-24 20:41:54 -0400

    Delicious graphs

--- server.py
+++ server.py
@@ -29,9 +29,10 @@ class ScoutServer(object):
 				<link href="http://fonts.googleapis.com/css?family=Chau+Philomene+One" rel="stylesheet" type="text/css">
          		<link href="/static/css/style.css" rel="stylesheet">
          		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-         		<script>if (typeof jQuery === 'undefined')
+         		<script>
+         		if (typeof jQuery === 'undefined')
 				  document.write(unescape('%3Cscript%20src%3D%22/static/js/jquery.js%22%3E%3C/script%3E'));
-				</script>
+         		</script>
 				<script type="text/javascript" src="/static/js/jquery.tablesorter.js"></script>
 				<script>
 				$(document).ready(function() {{
@@ -79,19 +80,27 @@ class ScoutServer(object):
 			sum = [0]*7
 		conn.close()
 		output = ''
+		dataset = []
 		for e in entries:
+			dp = {"match": e[1], "rc":0, "tote":0, "auto":0, "step":0}
 			a = ''
 			if e[5]: #stacked?
 				a += "3 tote; "
+				dp['auto'] += 20;
 			elif e[2] == 3: #number of totes
 				a += "3 tote, not stacked; "
+				dp['auto'] += 6
 			elif e[2]:
 				a += str(e[2]) + ' totes; '
+				dp['auto'] += 2*e[2]
 			if e[3]:
+				dp['auto'] += 8/3 * e[3]
 				a += str(e[3]) +  ' RC(s) in zone; '
 			if e[4]:
+				dp['step'] = e[4]
 				a += str(e[4]) + ' RC(s) from step; '
 			if e[6]:
+				dp['auto'] += 4/3
 				a += 'moved into auto zone; '
 			s = ''
 			for stack in range(7, 13):
@@ -100,10 +109,15 @@ class ScoutServer(object):
 				st = literal_eval(e[stack])
 				if st['capping']:
 					s += 'capped '
+					dp['rc'] += st['height']*4
+				else:
+					dp['tote'] += st['height']*2
 				s += str(st['height']) + ' stack'
 				if st['capped']:
+					dp['rc'] += st['height']*4
 					s += ', capped'
 				if st['noodled']:
+					dp['rc'] += 6
 					s += '/noodled'
 				s += '; '
 
@@ -131,14 +145,132 @@ class ScoutServer(object):
 				<td>{2}</td>
 				<td>{3}</td>
 			</tr>'''.format(e[1], a[:-2], s[:-2], o)
+			for key,val in dp.items():
+				dp[key] = round(val, 2)
+			dataset.append(dp)
 
-
+		dataset.reverse()
 		return '''
 		<html>
 			<head>
 				<title>PiScout</title>
 				<link href="http://fonts.googleapis.com/css?family=Chau+Philomene+One" rel="stylesheet" type="text/css">
          		<link href="/static/css/style.css" rel="stylesheet">
+         		<script type="text/javascript" src="/static/js/amcharts.js"></script>
+				<script type="text/javascript" src="/static/js/serial.js"></script>
+				<script>
+					var chart;
+					var graph;
+
+					var chartData = {8};
+
+					AmCharts.ready(function () {{
+						// SERIAL CHART
+						chart = new AmCharts.AmSerialChart();
+
+						chart.dataProvider = chartData;
+						chart.marginLeft = 10;
+						chart.categoryField = "match";
+
+						// AXES
+						// category
+						var categoryAxis = chart.categoryAxis;
+						categoryAxis.dashLength = 3;
+						categoryAxis.minorGridEnabled = true;
+						categoryAxis.minorGridAlpha = 0.1;
+
+						// value
+						var valueAxis = new AmCharts.ValueAxis();
+						valueAxis.position = "left";
+						valueAxis.axisColor = "#111111";
+						valueAxis.gridAlpha = 0;
+						valueAxis.axisThickness = 2;
+						chart.addValueAxis(valueAxis)
+
+						var valueAxis2 = new AmCharts.ValueAxis();
+						valueAxis2.position = "right";
+						valueAxis2.axisColor = "#FCD202";
+						valueAxis2.gridAlpha = 0;
+						valueAxis2.axisThickness = 2;
+						chart.addValueAxis(valueAxis2);
+
+						// GRAPH
+						graph = new AmCharts.AmGraph();
+						graph.title = "RC/Noodle Points";
+						graph.valueAxis = valueAxis;
+						graph.type = "smoothedLine"; // this line makes the graph smoothed line.
+						graph.lineColor = "#637bb6";
+						graph.bullet = "round";
+						graph.bulletSize = 8;
+						graph.bulletBorderColor = "#FFFFFF";
+						graph.bulletBorderAlpha = 1;
+						graph.bulletBorderThickness = 2;
+						graph.lineThickness = 2;
+						graph.valueField = "tote";
+						graph.balloonText = "Tote Points:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+						chart.addGraph(graph);
+
+						graph2 = new AmCharts.AmGraph();
+						graph2.title = "Tote Points";
+						graph2.valueAxis = valueAxis;
+						graph2.type = "smoothedLine"; // this line makes the graph smoothed line.
+						graph2.lineColor = "#187a2e";
+						graph2.bullet = "round";
+						graph2.bulletSize = 8;
+						graph2.bulletBorderColor = "#FFFFFF";
+						graph2.bulletBorderAlpha = 1;
+						graph2.bulletBorderThickness = 2;
+						graph2.lineThickness = 2;
+						graph2.valueField = "rc";
+						graph2.balloonText = "RC Points:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+						chart.addGraph(graph2);
+
+						graph3 = new AmCharts.AmGraph();
+						graph3.title = "Auto Points";
+						graph3.valueAxis = valueAxis;
+						graph3.type = "smoothedLine"; // this line makes the graph smoothed line.
+						graph3.lineColor = "#FF6600";
+						graph3.bullet = "round";
+						graph3.bulletSize = 8;
+						graph3.bulletBorderColor = "#FFFFFF";
+						graph3.bulletBorderAlpha = 1;
+						graph3.bulletBorderThickness = 2;
+						graph3.lineThickness = 2;
+						graph3.valueField = "auto";
+						graph3.balloonText = "Auto Points:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+						chart.addGraph(graph3);
+
+						graph4 = new AmCharts.AmGraph();
+						graph4.valueAxis = valueAxis2;
+						graph4.title = "Step RCs";
+						graph4.type = "smoothedLine"; // this line makes the graph smoothed line.
+						graph4.lineColor = "#FCD202";
+						graph4.bullet = "round";
+						graph4.bulletSize = 8;
+						graph4.bulletBorderColor = "#FFFFFF";
+						graph4.bulletBorderAlpha = 1;
+						graph4.bulletBorderThickness = 2;
+						graph4.lineThickness = 2;
+						graph4.valueField = "step";
+						graph4.balloonText = "RCs from Step:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+						chart.addGraph(graph4);
+
+						// CURSOR
+						var chartCursor = new AmCharts.ChartCursor();
+						chartCursor.cursorAlpha = 0;
+						chartCursor.cursorPosition = "mouse";
+						chart.addChartCursor(chartCursor);
+
+						var legend = new AmCharts.AmLegend();
+						legend.marginLeft = 110;
+						legend.useGraphSettings = true;
+						chart.addLegend(legend);
+						chart.creditsPosition = "bottom-right";
+
+						// WRITE
+						chart.write("chartdiv");
+					}});
+				</script>
 			</head>
 			<body>
 				<h1>Team {0}</h1>
@@ -159,6 +291,8 @@ class ScoutServer(object):
 					</div>
 				</div>
 				<br>
+				<div id="chartdiv" style="width:1000px; height:400px; margin: 0 auto;"></div>
+				<br>
 				<table>
 					<thead><tr>
 						<th>Match</th>
@@ -168,7 +302,7 @@ class ScoutServer(object):
 					</tr></thead>{1}
 				</table>
 			</body>
-		</html>'''.format(n, output, sum[1], sum[2], sum[3], sum[4], sum[5], sum[6])
+		</html>'''.format(n, output, sum[1], sum[2], sum[3], sum[4], sum[5], sum[6], str(dataset).replace("'",'"'))
 
 	@cherrypy.expose
 	def submit(self, data=''):
