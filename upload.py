9d772c8a4f19596978e2badedf1b2ea0544ee516
Kevin O'Connor <koconnor@usfirst.org>
2018-05-24 11:20:23 -0400

    Fix up upload.py

--- upload.py
+++ upload.py
@@ -6,16 +6,31 @@ import serverinfo
 n = 1
 print("Attemting to upload matches...")
 if os.path.isfile('queue.txt'):
-	with open("queue.txt", "r") as file:
-		try:
+	try:
+    with open("queue.txt", "r") as file:
 			for line in file:
 				requests.post(serverinfo.server + "/submit", data={'event':server.CURRENT_EVENT, 'data': line, 'auth': serverinfo.AUTH})
 				print("Uploaded entry number " + str(n))			
 				n += 1
-		except:
-			print("Failed miserably. Are you connected to the internet?")
-	os.remove('queue.txt')
+    os.remove('queue.txt')  
+  except:
+    print("Failed miserably. Are you connected to the internet?")
 else:
 	print("The match queue doesn't exist.")
+  
+n=0
+if os.path.isfile('pitQueue.txt'):
+	try:
+    with open("pitQueue.txt", "r") as file:
+			for line in file:
+				requests.post(serverinfo.server + "/submit", data={'event':server.CURRENT_EVENT, 'pitData': line, 'auth': serverinfo.AUTH})
+				print("Uploaded pit entry number " + str(n))			
+				n += 1
+    os.remove('pitQueue.txt')  
+  except:
+    print("Failed miserably. Are you connected to the internet?")
+else:
+	print("The pit queue doesn't exist.")
+    
 print("Finished.")
 input("Press Enter to exit")
